a77517854808abcc902b6c6315b85457760866fe
Guadalupe Chasiquiza <mchasiquizac@est.ups.edu.ec>
2023-01-15 22:06:22 -0500

    Validación y colores

--- personadialog.cpp
+++ personadialog.cpp
@@ -47,9 +47,54 @@ void PersonaDialog::on_buttonBox_accepted()
     QString email =  ui->inEmail->text();
 
     if (nombre.isEmpty()|| apellido.isEmpty() || telefono.isEmpty() || email.isEmpty()){
-        QMessageBox::warning(this, "Adventencia", "Todos los campos deben estar llenos");
+        QMessageBox::warning(this,"Advertencia","Debe completar todos los mensajes de texto");
         return;
     }
+
+    for(int i=0; i<nombre.length(); i++)
+    {
+        if(nombre[i].isDigit())
+        {
+            QMessageBox::warning(this,"Advertencia","El campo de texto\"Nombre\"solo admite letras");
+            return;
+        }
+    }
+
+    for (int i=0; i<apellido.length(); i++)
+    {
+        if(apellido[i].isDigit())
+        {
+            QMessageBox::warning(this,"Advertencia","El campo de texto\"Apellido\"solo admite letras");
+            return;
+        }
+    }
+
+    for (int i=0; i<telefono.length(); i++)
+    {
+        if(!telefono[i].isDigit())
+        {
+            QMessageBox::warning(this,"Advertencia","El campo de texto\"Teléfono\"solo admite números");
+            return;
+        }
+    }
+    bool a=false, b=false;
+    for (int i=0; i<email.length(); i++)
+    {
+        if(email[i]=="@")
+        {
+            a=true;
+        }
+        if(email[i]==".")
+        {
+           b=true;
+        }
+    }
+    if(a==false||b==false)
+    {
+       QMessageBox::warning(this,"Advertencia","El campo de texto\"email\"está incorrecto");
+       return;
+    }
+
     this->m_persona = new Persona(nombre, apellido, telefono, email);
     accept();
 }
